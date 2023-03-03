f924b9e9ff7c5b0e55f3eb61ba8ac4ae3d4cb547
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-03-03 11:27:19 +0000

    Reorder tests

--- src/test/kotlin/MaxArgsTest.kt
+++ src/test/kotlin/MaxArgsTest.kt
@@ -5,6 +5,14 @@ import org.junit.jupiter.api.Test
 
 class MaxArgsTest {
     @Test
+    fun `it returns 0 for an assignment statement`() {
+        assertEquals(
+            0,
+            maxArgs(AssignmentStatement("a", Number(1)))
+        )
+    }
+
+    @Test
     fun `it returns the number of arguments of a print statement`() {
         val statement =
             PrintStatement(
@@ -17,13 +25,5 @@ class MaxArgsTest {
         val result = maxArgs(statement)
 
         assertEquals(2, result)
-    }
-
-    @Test
-    fun `it returns 0 for an assignment statement`() {
-        assertEquals(
-            0,
-            maxArgs(AssignmentStatement("a", Number(1)))
-        )
     }
 }
