653b65ac79810398749d572554c5b52c913d4a6a
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-03-17 12:02:59 +0000

    Refactor to get a list of StatementThenExpressions

--- src/main/kotlin/com/compiler/maxArgs.kt
+++ src/main/kotlin/com/compiler/maxArgs.kt
@@ -2,30 +2,32 @@ package com.compiler
 
 fun maxArgs(statement: Statement): Int =
     when (statement) {
-        is PrintStatement ->
-            when (val statementThenExpression = statement.expressions.aStatementThenExpression()) {
+        is PrintStatement -> {
+            when (val statementThenExpression = statement.expressions.statementThenExpressions().firstOrNull()) {
                 null -> statement.expressions.size()
                 else -> maxOf(
                     statement.expressions.size(),
                     maxArgs(statementThenExpression.statement)
                 )
             }
+        }
+
         is AssignmentStatement -> 0
         is CompoundStatement -> maxOf(maxArgs(statement.statement1), maxArgs(statement.statement2))
     }
 
-private fun ExpressionList.aStatementThenExpression(): StatementThenExpression? {
+private fun ExpressionList.statementThenExpressions(): List<StatementThenExpression> {
     return when (this) {
         is LastExpressionList ->
             when (expression) {
-                is StatementThenExpression -> expression
-                else -> null
+                is StatementThenExpression -> listOf(expression)
+                else -> listOf()
             }
 
         is PairExpressionList ->
             when (head) {
-                is StatementThenExpression -> head
-                else -> tail.aStatementThenExpression()
+                is StatementThenExpression -> listOf(head)
+                else -> tail.statementThenExpressions()
             }
     }
 }
