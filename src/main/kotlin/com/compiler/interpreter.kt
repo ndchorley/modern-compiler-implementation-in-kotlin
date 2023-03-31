fb9748dc56006a3bb711d0659d46e322a2470b11
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-03-31 13:49:32 +0100

    Introduce an internal State model for the interpreter

--- src/main/kotlin/com/compiler/interpreter.kt
+++ src/main/kotlin/com/compiler/interpreter.kt
@@ -2,17 +2,26 @@ package com.compiler
 
 fun interpret(statement: Statement): String =
     when (statement) {
-        is PrintStatement -> interpret(statement.expressions)
+        is PrintStatement -> interpret(statement.expressions).output
         else -> TODO()
     }
 
-private fun interpret(expressionList: ExpressionList): String =
+data class State(val output: String)
+
+private fun interpret(expressionList: ExpressionList): State =
     when (expressionList) {
-        is LastExpressionList -> interpret(expressionList.expression).plus("\n")
-        is PairExpressionList ->
-            interpret(expressionList.head)
-                .plus(" ")
-                .plus(interpret(expressionList.tail))
+        is LastExpressionList -> {
+            val output = interpret(expressionList.expression).plus("\n")
+            State(output)
+        }
+        is PairExpressionList -> {
+            val output =
+                interpret(expressionList.head)
+                    .plus(" ")
+                    .plus(interpret(expressionList.tail).output)
+
+            State(output)
+        }
     }
 
 private fun interpret(expression: Expression): String =
