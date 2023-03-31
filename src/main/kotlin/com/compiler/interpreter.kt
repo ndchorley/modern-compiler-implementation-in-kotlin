1733c8a2b3a0a7c3834b7888803fcb37ffd56dd2
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-03-31 15:11:32 +0100

    Refactor interpret(Expression) to return a State

--- src/main/kotlin/com/compiler/interpreter.kt
+++ src/main/kotlin/com/compiler/interpreter.kt
@@ -11,21 +11,25 @@ private data class State(val output: String)
 private fun interpret(expressionList: ExpressionList): State =
     when (expressionList) {
         is LastExpressionList -> {
-            val output = interpret(expressionList.expression).plus("\n")
-            State(output)
+            val newState = interpret(expressionList.expression)
+
+            newState.copy(output = newState.output.plus("\n"))
         }
+
         is PairExpressionList -> {
-            val output =
-                interpret(expressionList.head)
+            val newState = interpret(expressionList.head)
+
+            val newOutput =
+                newState.output
                     .plus(" ")
                     .plus(interpret(expressionList.tail).output)
 
-            State(output)
+            newState.copy(output = newOutput)
         }
     }
 
-private fun interpret(expression: Expression): String =
+private fun interpret(expression: Expression): State =
     when (expression) {
-        is Number -> expression.value.toString()
+        is Number -> State(expression.value.toString())
         else -> TODO()
     }
