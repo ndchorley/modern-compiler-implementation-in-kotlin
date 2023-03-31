6f715e86fe19ce363e9f949881e5a65e3ce2170b
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-03-31 10:36:44 +0100

    Refactor to get the first item from the expression list

--- src/main/kotlin/com/compiler/interpreter.kt
+++ src/main/kotlin/com/compiler/interpreter.kt
@@ -2,6 +2,18 @@ package com.compiler
 
 fun interpret(statement: Statement): String =
     when (statement) {
-        is PrintStatement -> "1\n"
+        is PrintStatement -> {
+            when (val expression = statement.expressions.first()) {
+                is Number -> "${expression.value}\n"
+                else -> TODO()
+            }
+        }
+
         else -> TODO()
+    }
+
+private fun ExpressionList.first(): Expression =
+    when (this) {
+        is LastExpressionList -> this.expression
+        is PairExpressionList -> TODO()
     }
